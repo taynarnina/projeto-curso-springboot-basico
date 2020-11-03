e8f33845ea1f639a085fdff4bfdd93466d7a6280
taynarnina <tayanrnian@gmail.com>
2020-11-03 16:11:38 -0300

    projeto cirado

---
+++ src/main/java/com/cursoexemplospring/CursoExemploSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cursoexemplospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoExemploSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoExemploSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoexemplospring/CursoExemploSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoexemplospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoExemploSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
